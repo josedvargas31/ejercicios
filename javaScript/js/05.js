84d9eaf2100ffdd33303042db7c08c1add6eb58e
josedvargas31 <dajozavargas@gmail.com>
2024-10-23 20:51:03 -0500

    inicializando el ejercicio cinco

---
+++ javaScript/js/05.js
@@ -0,0 +1,4 @@
+// Ejercicio 5: Contar vocales
+// Escribe una función llamada "contarVocales" que acepte una cadena de texto (palabra) 
+// y devuelva la cantidad de vocales (a, e, i, o, u) que contiene.
+// Ejemplo: contarVocales("javascript") debería devolver 3.
