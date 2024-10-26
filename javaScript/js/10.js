e2503bb00fe3c237cd6cf3d877d1d72dd20d2e0d
josedvargas31 <dajozavargas@gmail.com>
2024-10-25 22:11:10 -0500

    Inicializando el ejercicio diez de javaScript

---
+++ javaScript/js/10.js
@@ -0,0 +1,7 @@
+// Ejercicio 10: Multiplicar matrices
+// Crea una función llamada "multiplicarMatrices" que acepte dos matrices (arrays bidimensionales) 
+// y devuelva el resultado de su multiplicación. 
+// Recuerda que para multiplicar dos matrices, el número de columnas de la primera debe ser igual al número de filas de la segunda.
+// Ejemplo:
+// multiplicarMatrices([[1, 2], [3, 4]], [[5, 6], [7, 8]]) debería devolver [[19, 22], [43, 50]].
+
